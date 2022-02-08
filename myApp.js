97460d346128e1f3a05e29887bded4c76df7c36d
Gejix <geraldjuwah@gmail.com>
2022-02-08 11:53:23 +0100

    public route added

--- myApp.js
+++ myApp.js
@@ -4,6 +4,9 @@ var app = express();
 
 console.log("Hello World");
 
+// Assets at the /public route
+app.use("/public", express.static(__dirname + "/public"));
+
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/views/index.html");
   });
